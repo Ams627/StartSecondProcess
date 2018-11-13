20062c68e134a970f3ae9af5d1a62477f0a7bd87
Adrian Sims <adriansims62781@gmail.com>
2018-11-13 20:55:11 +0100

    Add project files.

---
+++ StartSecondProcess/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartSecondProcess
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var currentPaths = Environment.GetEnvironmentVariable("PATH").Split(';');
+            var newPath = currentPaths[0] + ";" + currentPaths.Skip(2).Aggregate((a, b) => a + ";" + b);
+            var newPath2 = "c:\\bin";
+
+            Console.WriteLine();
+            try
+            {
+                var processInfo = new ProcessStartInfo();
+                var vars = processInfo.EnvironmentVariables;
+                vars["PATH"] = newPath2;
+                Console.WriteLine();
+
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add StartSecondProcess/Program.cs
+git add StartSecondProcess/App.config
+git add StartSecondProcess/Properties/AssemblyInfo.cs
+git add StartSecondProcess/StartSecondProcess.csproj
+git add StartSecondProcess.sln
+git add .gitignore
